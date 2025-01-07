f10dd862a7b6b1351cc8d1628aadabc181056901
Ilya Korotaev <korotaevilv@icloud.com>
2025-01-07 19:25:12 +0400

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"ToDoGo"
+	"ToDoGo/pkg/handler"
+	"ToDoGo/pkg/repository"
+	"ToDoGo/pkg/service"
+	"github.com/spf13/viper"
+	"log"
+)
+
+func main() {
+	if initConfig() != nil {
+		log.Fatalf("config initializing failed: %s", initConfig().Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	srv := new(ToDoGo.Server)
+	err := srv.Run(viper.GetString("port"), handlers.InitRoutes())
+	if err != nil {
+		log.Fatalf("error while starting server: %e", err.Error())
+	} else {
+		log.Println("Start Server Success")
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("../configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
