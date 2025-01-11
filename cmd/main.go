f5165adffcd05d1896a0bcfaea8908bb3d0f5557
Ilya Korotaev <korotaevilv@icloud.com>
2025-01-12 00:04:56 +0400

    Commenting code Create readme.md

--- cmd/main.go
+++ cmd/main.go
@@ -13,45 +13,65 @@ import (
 )
 
 func main() {
+	// Инициализация конфигурации приложения
 	if initConfig() != nil {
 		log.Fatalf("config initializing failed: %s", initConfig().Error())
 	}
-	log.Default().Println("Config init  done")
+
+	// Загрузка переменных окружения из файла .env
 	if godotenv.Load(".env") != nil {
 		log.Fatalf("env initializing failed: %s", godotenv.Load(".env").Error())
 	}
+
+	// Создание соединения с базой данных PostgreSQL с использованием конфигурации
 	db, err := repository.NewPostgresDB(repository.ConnConfig{
-		Host:     viper.GetString("database.host"),
-		Port:     viper.GetString("database.port"),
-		Username: viper.GetString("database.username"),
-		Password: os.Getenv("DB_PASSWORD"),
-		Database: viper.GetString("database.database"),
-		SSLMode:  viper.GetString("database.sslmode"),
-		//Host: "localhost", Port: "5436", Username: "postgres", Password: "qwerty", Database: "postgres", SSLMode: "disable",
+		Host:     viper.GetString("database.host"),     // Хост базы данных
+		Port:     viper.GetString("database.port"),     // Порт базы данных
+		Username: viper.GetString("database.username"), // Имя пользователя базы данных
+		Password: os.Getenv("DB_PASSWORD"),             // Пароль базы данных из переменных окружения
+		Database: viper.GetString("database.database"), // Имя базы данных
+		SSLMode:  viper.GetString("database.sslmode"),  // Режим SSL для базы данных
 	})
 
+	// Проверка на наличие ошибок при инициализации базы данных
 	if err != nil {
 		log.Fatalf("database initializing failed: %s", err.Error())
 	}
+
+	// Логирование успешной инициализации базы данных
 	log.Default().Println("DB init done")
+
+	// Создание нового репозитория на основе соединения с базой данных
 	repos := repository.NewRepository(db)
 	log.Default().Println("Repository init done")
+
+	// Создание нового сервиса на основе репозитория
 	services := service.NewService(repos)
 	log.Default().Println("Service init done")
+
+	// Создание нового обработчика на основе сервиса
 	handlers := handler.NewHandler(services)
 	log.Default().Println("Handler init done")
+
+	// Создание нового экземпляра сервера
 	srv := new(ToDoGo.Server)
+
+	// Запуск сервера и инициализация маршрутов
 	err = srv.Run(viper.GetString("srvport"), handlers.InitRoutes())
+
+	// Проверка на ошибки при запуске сервера
 	if err != nil {
 		log.Fatalf("error while starting server: %e", err.Error())
 	} else {
-		log.Println("Start Server Success")
+		log.Println("Start Server Success") // Успешный запуск сервера
 	}
-	log.Default().Println("DB done")
+
+	log.Default().Println("DB done") // Завершение работы с базой данных
 }
 
+// Функция для инициализации конфигурации приложения
 func initConfig() error {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
+	viper.AddConfigPath("configs") // Установка пути к конфигурационным файлам
+	viper.SetConfigName("config")  // Установка имени конфигурационного файла
+	return viper.ReadInConfig()    // Чтение конфигурации из файла
 }
--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -7,42 +7,37 @@ import (
 )
 
 func (h *Handler) signUp(c *gin.Context) {
-	var input models.User
-
-	if err := c.BindJSON(&input); err != nil {
-		//log.Fatalf()
+	var input models.User                      // получаем на вход структуру User и валидируем запрос
+	if err := c.BindJSON(&input); err != nil { // записываем данные из JSON по сссылке input'а
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
-	id, err := h.services.Authorization.CreateUser(input)
+	id, err := h.services.Authorization.CreateUser(input) // отправляем введенные данные на уровень сервиса
 	if err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		newErrorResponse(c, http.StatusInternalServerError, err.Error()) // если ответ кривой возвращаем ошибку
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"id": id})
+	c.JSON(http.StatusOK, gin.H{"id": id}) // иначе возвращаем айдишник пользователя
 }
 
-type signInInput struct {
+type signInInput struct { //Кастом структура, так как структура User подразумевает обязательное имя
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
 func (h *Handler) signIn(c *gin.Context) {
-
 	var input signInInput
-
-	if err := c.BindJSON(&input); err != nil {
-		//log.Fatalf()
+	if err := c.BindJSON(&input); err != nil { // если тело запроса кривое, то возвращаем Bad Request Error
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
-	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password) // если все на кондициях то отправляем логи на уровень сервиса
 	if err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		newErrorResponse(c, http.StatusInternalServerError, err.Error()) // обрабатываем ошибку ответа
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"token": token})
+	c.JSON(http.StatusOK, gin.H{"token": token}) // возвращаем токен
 
 }
